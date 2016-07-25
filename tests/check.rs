c343143c70efa27a48082fce11f4c11c367c017f
Sindre Stephansen <sindre@sindrestephansen.com>
2016-07-25 12:39:33 +0200

    Added logging to tests/check.rs.

--- tests/check.rs
+++ tests/check.rs
@@ -1,8 +1,14 @@
 extern crate chess;
+extern crate env_logger;
 use chess::*;
+
+fn log() {
+    let _ = env_logger::init();
+}
 
 #[test]
 fn basic() {
+    log();
     let mut game = Game::new_empty();
     game.set_at_pos((0,0), Some(&WHITE[5]));
     game.set_at_pos((1,1), Some(&BLACK[4]));
